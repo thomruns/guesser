cd26b53c8a77fb80ba608f2a9fbeac211499a2e0
Thom David <thomdavid74@gmail.com>
2018-03-19 11:56:48 -0700

    Input validation, message function

--- app.js
+++ app.js
@@ -24,3 +24,31 @@ const game = document.querySelector('#game'),
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+  // console.log(guess); //Testing purposes only
+  
+  // Validate input
+  if(isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, 'green');
+  }
+  // Check if won
+  if(guess === winningNum) {
+    // Disable input
+    guessInput.disabled = true;
+    // Change border to green
+    guessInput.style.borderColor = 'green';
+    // Set message to winner
+    setMessage(`${winningNum} is correct...you win!`, 'green');
+  } else {
+
+  }
+});
+
+// Set message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
